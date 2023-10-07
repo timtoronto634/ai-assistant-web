3976285f88cb05dd4c53378b8bcefb920de08178
timtoronto634 <tim225x@gmail.com>
2023-10-07 16:09:57 +0900

    wip change initial code

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# Setup
+
+```
+pnpm install
+pnpm dev
+```
+
 # React + TypeScript + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button, TextareaAutosize } from '@mui/material'
+import ButtonUsage from './components/button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,21 +11,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <TextareaAutosize />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          You are a senior software engineer
         </p>
+        <ButtonUsage />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
