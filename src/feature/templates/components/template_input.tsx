bc39c5d5c14fb90fdd756fa1ef03f9a144a57810
timtoronto634 <tim225x@gmail.com>
2023-12-13 08:33:28 +0900

    chore: indent

--- src/feature/templates/components/template_input.tsx
+++ src/feature/templates/components/template_input.tsx
@@ -3,14 +3,14 @@ import TextField from '@material-ui/core/TextField/';
 import { TemplateBox } from './template_box';
 
 export const TemplateInput = () => {
-    const handleTemplateClick = (template: string) => {
-      console.log(template)
-    };
+  const handleTemplateClick = (template: string) => {
+    console.log(template)
+  };
 
-    return (
-      <div style={{ maxWidth: '400px', margin: 'auto' }}>
-        <TemplateBox template="Template 1" onTemplateClick={handleTemplateClick} />
-        <TemplateBox template="Template 2" onTemplateClick={handleTemplateClick} />
-      </div>
-    );
+  return (
+    <div style={{ maxWidth: '400px', margin: 'auto' }}>
+      <TemplateBox template="Template 1" onTemplateClick={handleTemplateClick} />
+      <TemplateBox template="Template 2" onTemplateClick={handleTemplateClick} />
+    </div>
+  );
 }
